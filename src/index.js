f7a61339777fdc74ffc8aafffc80ebb5dc544ccd
Mikalai Stadub <mikalaistadub@gmail.com>
2020-10-01 21:58:05 +0300

    feat: add function min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if(!array || array.length == 0){
+    return 0;
+  }
+  let len = array.length;
+  let min = Infinity;
+  while (len--) {
+      if (array[len] < min) {
+          min = array[len];
+      };
+  };
+  return min;
+};
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || array.length == 0){
+    return 0;
+  }
+  let len = array.length;
+  let max = -Infinity;
+  while (len--) {
+      if (array[len] > max) {
+          max = array[len];
+      }
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    let sum = 0;
+    if(!array || array.length == 0){
+    return 0;
+  };
+
+    for (let i = 0; i < array.length; i ++){
+     sum = sum + array[i];
+    }
+  return sum/array.length;
 }
